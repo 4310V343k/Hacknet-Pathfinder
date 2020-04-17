f93903ac4f50eaf0c8533be2d9df168c611f66dc
Arkhist <peyrille.benjamin@gmail.com>
2020-04-17 16:16:23 +0200

    Add PF extension parsing functionalities

--- Pathfinder/Extension/PathfinderExtensionInfo.cs
+++ Pathfinder/Extension/PathfinderExtensionInfo.cs
@@ -1,13 +1,27 @@
-﻿using Hacknet.Extensions;
+﻿using System;
+using Hacknet.Extensions;
+using Pathfinder.Util.XML;
 
 namespace Pathfinder.Extension
 {
     public class PathfinderExtensionInfo : ExtensionInfo
     {
+        public bool PathfinderExt { get; protected set; } = false;
+
         public static PathfinderExtensionInfo CloneHacknetInfo(ExtensionInfo info)
         {
             PathfinderExtensionInfo result = new PathfinderExtensionInfo();
             return Util.Utility.ObjectMap(result, info);
+        }
+
+        public void ParseInfoFromFolderPath(string folderpath)
+        {
+            var executor = new EventExecutor(folderpath + "/ExtensionInfo.xml", ignoreCase: true);
+            executor.AddExecutor("Pathfinder", (exec, info) =>
+            {
+                this.PathfinderExt = true;
+            });
+            executor.Parse();
         }
     }
 }
